50b8c41a570691a86212509826285f63cda82d3c
Saida Davis <saihdavis@gmail.com>
2020-05-07 05:29:22 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,10 +7,16 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != self
+      raise PartnerError
+    else
+      person.partner = self 
   end
-
-end
+end 
+  
+  class  PartnerError < StandardError
+    end
+  end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
